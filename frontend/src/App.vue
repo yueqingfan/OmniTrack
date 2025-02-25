9907280121240e0f47880c6c6dca856231ab9cd3
yueqingfan <yueqingfan@outlook.com>
2025-02-25 15:43:05 +0800

    connect front and back

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="web">
+    <h1>公共安全监控系统</h1>
+    <div>
+      <h3>检测结果：</h3>
+      <ul>
+        <li >
+          {{ result }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style>
+#web {
+  font-family:Avenir,Helvettica,Arial,ans-serif;
+  margin:60px;
+}
+</style>
