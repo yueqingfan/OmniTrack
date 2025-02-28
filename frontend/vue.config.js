557b85dfebbe1442bafad801cab7f6499f7b4ed4
yueqingfan <yueqingfan@outlook.com>
2025-02-28 15:33:15 +0800

    deploy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-      ? '/OmniTrack/frontend/'  // 仓库名为 OmniTrack
+      ? '/OmniTrack/'
       : '/',
   devServer: {
     host: '0.0.0.0',
